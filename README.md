d8e2d58bfa4244283108a2accdba2657fb844d54
众触应用平台 <87555332+zclc@users.noreply.github.com>
2021-07-17 18:50:05 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# 批量上传视频
+
+众触零码应用平台插件
+
+Plugins For ZhongChu ZeroCode Application Platform
---
+++ plugins/zp120_批量上传视频.js
@@ -0,0 +1,121 @@
+import React from "react"
+import css from "../css/zp120_批量上传视频.css"
+
+function render(ref) {
+    const { exc, render, props } = ref
+    if (!props.dbf) return <div>请配置表单字段</div>
+    let uploaded = ref.getForm(props.dbf)
+    if (!Array.isArray(uploaded)) {
+        if (uploaded) warn("表单字段必须是数组")
+        uploaded = []
+    }
+    return <React.Fragment>
+        {EL.网盘}
+        <div className="zp120B">
+            <div>{EL.camera}<label>{props.label || "上传视频"}</label></div>
+            <input onChange={e => onChange(ref, e)} type="file" accept="video/*" multiple="multiple"/>
+        </div>
+        {ref.uploading.map((a, i) => <div className={"zp120B zp120U zp120_" + i} key={i}><div/><video src={a}/></div>)}
+        {uploaded.map((a, i) => <div className="zp120B zp120Z" onClick={() => {ref.zoom = a; render()}} key={a + i}>
+            <img src={a + "?x-oss-process=video/snapshot,m_fast,t_5000,w_0,ar_auto"}/><i className="zplaybtn"/>
+            <span onClick={e => {e.stopPropagation(); exc('confirm("确定要删除吗？")', {}, () => {uploaded.splice(i, 1); ref.setForm(props.dbf, uploaded)})}}>{EL.del}</span>{EL.handle}
+        </div>)}
+        {ref.zoom && <div onClick={() => {delete ref.zoom; render()}} className="zmask"><video src={ref.zoom} controls="controls" autoPlay="autoplay"/>{EL.del}</div>}
+    </React.Fragment>
+}
+
+function onInit(ref) {
+    const { id, exc, props, render } = ref
+    ref.uploading = []
+    if (props.gallery) {
+        EL.网盘 = render({ t: "Plugin", p: { local: true, ID: "zp101", P: { mineOnly: true, onSelect: '$("#' + id + '").add($x)', type: "v", label: "视频库" } } }, id + "_0")
+        ref.container.add = url => {
+            let arr = ref.getForm(props.dbf)
+            if (!Array.isArray(arr)) arr = []
+            arr.unshift(url)
+            ref.setForm(props.dbf, arr)
+        }
+    }
+    exc('load("//z.zcwebs.cn/vendor/Sortable_1.13.0.js")', {}, () => {
+        new Sortable(ref.container, {
+            animation: 150,
+            forceFallback: true,
+            fallbackTolerance: 5,
+            onSort: e => {
+                let arr = ref.getForm(props.dbf)
+                if (!Array.isArray(arr)) arr = []
+                arr.splice(e.newDraggableIndex, 0, arr.splice(e.oldDraggableIndex, 1)[0])
+                ref.setForm(props.dbf, arr)
+            },
+            handle: "#" + id + " .zp118handler",
+            draggable: ".zp120Z",
+            dragClass: "zp120Drag",
+            ghostClass: "zp120Drop"
+        })
+    })
+}
+
+function onChange(ref, e) {
+    const { exc, render, props } = ref
+    const arr = Array.from(e.target.files)
+    if (!arr.length) return exc('warn("请选择视频")')
+    arr.forEach((file, i) => setTimeout(() => {
+        const x = URL.createObjectURL(file)
+        ref.uploading.push(x)
+        render()
+        exc('upload(file, onProgress, onSuccess, onError)', {
+            file,
+            onProgress: r => {
+                $("#" + ref.id + " .zp120_" + ref.uploading.indexOf(x) + " div").innerHTML = r.percent + "%"
+            },
+            onSuccess: r => {
+                ref.uploading.splice(ref.uploading.indexOf(x), 1)
+                let arr = ref.getForm(props.dbf)
+                if (!Array.isArray(arr)) arr = []
+                arr.unshift(r.url)
+                ref.setForm(props.dbf, arr)
+                URL.revokeObjectURL(x)
+            },
+            onError: r => {
+                exc(`alert("上传出错了", r.error)`, { r })
+                ref.uploading.splice(ref.uploading.indexOf(x), 1)
+                URL.revokeObjectURL(x)
+                exc('render()')
+            }
+        })
+    }, 2000 * i))
+}
+
+$plugin({
+    id: "zp120",
+    props: [{
+        prop: "dbf",
+        type: "text",
+        label: "表单字段"
+    }, {
+        prop: "label",
+        type: "text",
+        label: "[上传视频]文本"
+    }, {
+        prop: "gallery",
+        type: "switch",
+        label: "包含视频库"
+    }],
+    render,
+    onInit,
+    css
+})
+
+const EL = {
+    camera: <svg className="zsvg zp120camera" viewBox="64 64 896 896"><path d="M912 302.3L784 376V224c0-35.3-28.7-64-64-64H128c-35.3 0-64 28.7-64 64v576c0 35.3 28.7 64 64 64h592c35.3 0 64-28.7 64-64V648l128 73.7c21.3 12.3 48-3.1 48-27.6V330c0-24.6-26.7-40-48-27.7zM712 792H136V232h576v560zm176-167l-104-59.8V458.9L888 399v226zM208 360h112c4.4 0 8-3.6 8-8v-48c0-4.4-3.6-8-8-8H208c-4.4 0-8 3.6-8 8v48c0 4.4 3.6 8 8 8z"/></svg>,
+    handle: <svg className="zsvg zp118handler" viewBox="0 0 1024 1024"><path d="M512 512M190.272 248.512l643.52 0 0 74.944-643.52 0 0-74.944ZM190.272 474.496l643.52 0 0 74.944-643.52 0 0-74.944ZM190.272 700.544l643.52 0 0 74.944-643.52 0 0-74.944Z"/></svg>,
+    del: <svg className="zsvg zp118del" viewBox="64 64 896 896"><path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 0 0 203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"/></svg>
+}
+
+
+/*
+zp120Z: zoom in/out
+zp120B: block
+zp120U: uploading
+
+*/
